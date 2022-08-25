3f026512c44f23489dff1b5b148986faad80ee9b
curtiende <109224646+curtiende@users.noreply.github.com>
2022-08-25 11:43:59 +0300

    Css changes to about file

--- front/src/container/About/About.js
+++ front/src/container/About/About.js
@@ -11,9 +11,24 @@ export default function About() {
         <div className="about-image"></div>
         <div className="about-text">
           <div className="about-text-top">
-            <p>I am a front-end web developer with more than two years knowledge of React Js, JavaScript, HTML, and CSS. I also have the basics of Java and python. I have enough knowledge of algorithms.I am a fast learner willing to work in a team to build web applications. I am also ready to learn more technologies as they emerge.</p>
+            <p>
+              I am a front-end web developer with more than two years knowledge
+              of React Js, JavaScript, HTML, and CSS. I also have the basics of
+              Java and python. I have enough knowledge of algorithms.I am a fast
+              learner willing to work in a team to build web applications. I am
+              also ready to learn more technologies as they emerge.
+            </p>
           </div>
-          <div className="about-text-list"></div>
+          <div className="about-text-list">
+            <b>Here are some of my skills</b>
+            <ul>
+              <li>Front-end web and mobile app development with React Js</li>
+              <li>Responsive development with css and javascript and HTML</li>
+              <li>Web development requirement specification</li>
+              <li>JSX Technology and Version control with Github</li>
+              <li>Software Testing and quality assurance</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
